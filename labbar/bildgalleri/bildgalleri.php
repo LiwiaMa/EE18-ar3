4456e88fabeb77525d64358c548acfb8436a6647
Liwia Matuszczak <liwia.matuszczak@learnet.se>
2020-11-16 09:17:53 +0100

    bildgalleri, slide

--- labbar/bildgalleri/bildgalleri.php
+++ labbar/bildgalleri/bildgalleri.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bildgalleri</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>b
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="kontainer">
+    <h1>Bildgalleri</h1>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -23,40 +26,17 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
             </ol>
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-
             <?php
             // Ange katalogen
             $katalog = "./bilder/";
-
-            echo "<h1>Bildgalleri</h1>";
 
             // Hämta katalogens innehåll
             $filer = scandir($katalog);
 
             // Skapa en div för grid
-            echo "<div class=\"kol4\">";
-
             // Loopa igenom alla funna filer
-            foreach ($filer as $fil) {
 
+            foreach ($filer as $key => $fil) {
                 // Visa bara bilden om filformat ”jpg”
 
                 if ($fil == "." || $fil == "..") {
@@ -68,10 +48,29 @@
                 //var_dump($info["extention"]);
 
                 if ($info["extension"] == "jpg") {
-                    echo "<img src=\"$katalog/$fil\" alt=\"\">";
+
+                    // Bara första bilden är active
+                    if ($key == 2) {
+                        echo "<div class=\"carousel-item active\">";
+                    } else {
+                        echo "<div class=\"carousel-item \">";
+                    }
+                    
+                    echo "<img  src=\"$katalog/$fil\" alt=\"\">";
+                    echo "</div>";
                 }
             }
             ?>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+
         </div>
 
     </div>
