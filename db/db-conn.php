ca837ddf20a771f923e8ae8b693b552e00e25d67
Liwia Matuszczak <liwia.matuszczak@learnet.se>
2020-11-23 11:17:45 +0100

    databas

---
+++ db/db-conn.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* PHP version 7
+* @category   
+* @author     Liwia Matuszczak <liwiamatuszczak.@gmail.com>
+* @license    PHP CC
+*/
+
+$user = "liwia";
+$db = "liwia";
+$host = "localhost";
+$pass = "Qx9LCr6KTnQZljy4";
+?>
