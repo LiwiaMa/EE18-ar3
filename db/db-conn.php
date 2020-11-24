91238d745c1a687733a5fde73bb15d7ab283c11b
Liwia Matuszczak <liwia.matuszczak@learnet.se>
2020-11-24 15:03:30 +0100

    db, sql

--- db/db-conn.php
+++ db/db-conn.php
@@ -10,4 +10,36 @@ $user = "liwia";
 $db = "liwia";
 $host = "localhost";
 $pass = "Qx9LCr6KTnQZljy4";
+
+// Logga in på mysql-server och välj databas (conn = connection/handtag)
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Gick det ansluta?
+if ($conn->connect_error) {
+    die("Kunde inte ansluta: " . $conn->connect_error);
+} else {
+    echo "<p>Hurrah! Det gick bra att ansluta</p>";
+}
+
+// Ställ en sql-fråga
+$sql = "SELECT * FROM bilar";
+$result = $conn->query($sql);
+
+// Gick sql satsen att köra?
+if (!$result) {
+    die("Något blev fel ,ed sql-satsen")
+} else {
+    echo "<p> Lista på alla bilar kunde hämtas</p>";
+}
+
+// loopa igenom listan på bilar
+while ($rad = $result->fetch_assoc()){
+    echo "<p>$rad</p>";
+}
+
+// Stäng ned anslutningen 
+$conn->close();
+
+
+
 ?>
