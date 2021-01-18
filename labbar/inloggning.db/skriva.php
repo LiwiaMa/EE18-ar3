1cc667f77896882128b444b30caf34cd6288dab5
LiwiaMa <liwiamatuszczak@gmail.com>
2021-01-18 08:51:30 +0100

    k

--- labbar/inloggning.db/skriva.php
+++ labbar/inloggning.db/skriva.php
@@ -35,15 +35,15 @@ if (!isset($_SESSION["anman"])) {
     <ul class="nav nav-tabs">
                 <?php if (isset($_SESSION["anamn"])) { ?>
                     <li class="nav-item"><a class="nav-link" href="./logout.php">Logga ut</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="./lista.php">Lista</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./skriva.php">Skriv</a></li>
+                    <li class="nav-item"><a class="nav-link " href="./lista.php">Lista</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="./skriva.php">Skriv</a></li>
                     <li class="nav-item anamn"> <?php echo $_SESSION["anamn"] . " (". $_SESSION["antal"].")" ;?> 
                 </li>
                 <?php } else { ?>
                     <li class="nav-item"><a class="nav-link" href="./login.php">Logga in</a></li>
                     <li class="nav-item"><a class="nav-link" href="./registrera.php">Registrera</a></li>
                     <li class="nav-item"><a class="nav-link" href="./lasa.php">Läs</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="./sok.php">Sök</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./sok.php">Sök</a></li>
                 <?php } ?>
             </ul>
         </nav>
