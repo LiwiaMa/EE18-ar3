18faea90c32d1097859b73023d322cf479058379
LiwiaMa <liwiamatuszczak@gmail.com>
2021-02-25 15:11:41 +0100

    addeventlistener, if

---
+++ labbar/rep/uppgift1.js
@@ -0,0 +1,14 @@
+const eLosen = document.addEventListener(".lösen");  
+const eSvar = document.addEventListener(".svar");  
+const eKnapp = document.addEventListener(".button");  
+
+var telefonummer = "0704037890";
+var losen = elosen.value;
+eKnapp.addEventListener("click", function () {
+
+   if (eLosen.value == 9) {
+       eSvar.value = telefonummer;
+   } else {
+        eLosen = "fel";
+   }
+});
