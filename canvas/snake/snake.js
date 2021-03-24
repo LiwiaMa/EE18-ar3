7ee86c8026c620bc60c095f09e48e561e4718320
LiwiaMa <liwiamatuszczak@gmail.com>
2021-03-24 19:52:37 +0100

    timer

--- canvas/snake/snake.js
+++ canvas/snake/snake.js
@@ -136,3 +136,29 @@ function draw() {
 // call draw function every 100ms
 
 var game = setInterval(draw, 100);
+
+// Timer
+const timeH = document.querySelector("h1");
+var timeSecond = 60;
+
+displayTime(timeSecond);
+
+const countDown = setInterval(() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if (timeSecond <= 0 || timeSecond < 1) {
+        endTime();
+        clearInterval(countDown);
+    }
+},1000);
+
+function displayTime(second) {
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML = `${min<10 ? '0': ''}${min}:${sec<10?'0':''}${sec}`
+}
+function endTime() {
+    timeH.innerHTML = "TIME OVER";
+    clearInterval(game);
+    dead.play();
+}
