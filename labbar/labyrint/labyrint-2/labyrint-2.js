f79e2fd5ae8a93f4728c7664c37834a79a033673
LiwiaMa <liwiamatuszczak@gmail.com>
2021-03-29 11:15:33 +0200

    lanyrint, objekt, while, poäng

--- labbar/labyrint/labyrint-2/labyrint-2.js
+++ labbar/labyrint/labyrint-2/labyrint-2.js
@@ -1,5 +1,6 @@
 // Element vi använder
 const canvas = document.querySelector("canvas");
+const ePoints = document.querySelector("p");
 
 // Ställ storlek på cavas
 canvas.width = 1000;
@@ -8,7 +9,7 @@ canvas.height = 750;
 // Stlå på rit api
 var ctx = canvas.getContext("2d");
 
-// Skapa kartan (0 = hål i väggen, 1 = vägg)
+// Skapa labyrinten (0 = hål i väggen, 1 = vägg)
 var karta = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
@@ -28,48 +29,93 @@ var karta = [
 ];
 // Figur objekt
 var figur = {
-    x: 25, // figur.x = figurX
-    y: 75, // figur.y = figurY
+    rad: 1, // figur.x = figurX
+    kolumn: 0, // figur.y = figurY
     rotation: 0,
+    poäng: 0, 
     bild: new Image()
 }
 figur.bild.src = "../nyckelpiga.png";
 
+// objekt mynt
+var mynt = {
+    rad: 0,
+    kolumn: 0,
+    bild: new Image ()
+}
+mynt.bild.src = "../coin1.png";
+
+
 // Rita ut figuren
 function ritaFigur() {
     ctx.save();
-    ctx.translate(figur.x, figur.y);
+    ctx.translate(figur.kolumn * 50 + 25, figur.rad * 50 + 25);
     ctx.rotate(figur.rotation / 180 * Math.PI);
     ctx.drawImage(figur.bild, -25, -25, 50, 50);
     ctx.restore();
 }
 
+// Rita mynt
+function ritaMynt() {
+    ctx.drawImage(mynt.bild, mynt.kolumn * 50, mynt.rad * 50, 50, 50);
+}
+
+// Spawna ett mynt
+function spawnaMynt() {
+    // Onändlig loop
+    while (true) {
+        mynt.rad = Math.floor(Math.random() * 16);
+        mynt.kolumn =  Math.floor(Math.random() * 20);
+
+        // Avbryt när myntet 0 
+         // Om vi hittar en "1", rita ut en kloss (vägg)
+        if (karta[mynt.rad][mynt.kolumn] == 0) {
+            break;
+        }
+    }
+}
+
+// Plocka myntet, få poäng
+function plockaPoäng() {
+    // Är figuren är i samma ruta som myntet?
+    if (figur.rad == mynt.rad && figur.kolumn == mynt.kolumn) {
+        // Öka poäng
+        figur.poäng++;
+        ePoints.textContent = figur.poäng;
+
+        console.log(figur.poäng);
+        // Spawna om myntet
+        spawnaMynt();
+
+    }
+}
+
+spawnaMynt();
+
 // Rita kartan
 function ritaKartan() {
     // Loopa igenom raderna (man måste byta namn på loopen till tex j, för att annars så skulle det krocka med den inre loopen)
-    for (var j = 0; j < 15; j++) {
+    for (var rad = 0; rad < 15; rad++) {
 
         // Loopa igenom arrayen (kolumerna)
-        for (var i = 0; i < 20; i++) {
+        for (var kolumn = 0; kolumn < 20; kolumn++) {
             // console.log(i, karta[i]);
 
-            // Om vi hittar en "1", rita ut en kloss
-            var x = i * 50;
-            var y = j * 50;
-            if (karta[j][i] == 1) {
-                ctx.fillRect(x, y, 50, 50);
+            // Om vi hittar en "1", rita ut en kloss (vägg)
+            if (karta[rad][kolumn] == 1) {
+                ctx.fillRect(kolumn * 50, rad * 50, 50, 50);
             }
         }
     }
     // Loopa igenom arrayen
     for (var i = 0; i < 12; i++) {
-        console.log(i, karta[i]);
+        // console.log(i, karta[i]);
 
         // Om vi hittar en "1", rita ut en kloss
         var x = i * 50;
         var y = 0;
         if (karta[i] == 1) {
-            ctx.fillRect(x, y, 50, 50);
+            ctx.fillRect(kolumn, rad, 50, 50);
         }
     }
 }
@@ -84,6 +130,12 @@ function loopen() {
 
     ritaFigur();
 
+    ritaMynt();
+
+    // krocka med myntet och få poäng
+    plockaPoäng();
+    
+
     requestAnimationFrame(loopen);
 }
 // starta loopen 
@@ -92,21 +144,32 @@ loopen();
 window.addEventListener("keypress", function (e) {
 
     switch (e.code) {
-        case "Numpad5":  // Pil nedåt
-                figur.y += 50;
+        case "Numpad2":  // Pil nedåt
+        // Är det 0 (gång) i rutan nedanför?
+                if (karta[figur.rad + 1][figur.kolumn] == 0) {
+                    // Isåfall flytta dit
+                    figur.rad++;
+                }
                 figur.rotation = 180;
             break;
         case "Numpad8":  // Pil uppåt
-                figur.y -= 50;
+        // Är det 0 i rutan ovanför?
+            if (karta[figur.rad - 1][figur.kolumn] == 0) {
+                // Isåfall flytta dit
+                figur.rad--;
+            }
                 figur.rotation = 0;
             break;
         case "Numpad4":  // Pil höger
-
-                figur.x -= 50;
+                if (karta[figur.rad][figur.kolumn - 1] == 0) {
+                    figur.kolumn--;
+                }
                 figur.rotation = 270;
             break;
         case "Numpad6":  // Pil vänster
-                figur.x += 50;
+            if (karta[figur.rad][figur.kolumn + 1] == 0) {
+                figur.kolumn++;
+            }
                 figur.rotation = 90;
             break;
 
@@ -114,7 +177,7 @@ window.addEventListener("keypress", function (e) {
             break;
 
     }
-    console.log("kolumn: " + (figur.x - 25) / 50 + ", rad: " + (figur.y - 25) / 50);
+    console.log("Kolumn: " + figur.kolumn + ", rad:" + figur.rad);
 });
 
 
