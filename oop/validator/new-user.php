bff49db306dc1cb69d4940ea794d57f80de9f639
LiwiaMa <liwiamatuszczak@gmail.com>
2021-05-03 10:34:56 +0200

    preg_match, foreach

--- oop/validator/new-user.php
+++ oop/validator/new-user.php
@@ -5,27 +5,51 @@ $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
 
 // Användarnamnet måste vara 6-12 tecken långt, stora och små bokstäver, samt siffror
-$resultat = "";
+$errors = [];
 function validateUsername($data)
 {
-    global $resultat;
-    if (preg_match("/[a-zA- Z0-9] {6,12}", $data)) {
-         $resultat = "<p>&#10003; Innehåller a-z, A_Z, 0-9</p>";
-    } else {
-         $resultat = "<p>&#10005; Innehåller INTE a-z, 0-9, @ och -.</p>";
-    }
+    global $errors;
+    if (!preg_match("/[a-zA- Z0-9] {6,12}", $data)) {
+         $errors ['username'][] = "<p>&#10005; Innehåller inte a-z, A_Z, 0-9</p>";
+    } 
 }
+
 
 function validatePassword($data)
 {
-
+    global $errors;
+    if (!preg_match("/[a-zåäö]/", $data) > 0) {
+        $errors['password'][] = '&#10005;password must contain a least one lowercase character<br>';
+    }
+    if (!preg_match("/[A-ZÅÄÖ]/", $data) > 0) {
+        $errors['password'][] = '&#10005; password must contain a least one uppercase character<br>';
+    }
+    if (!preg_match("/[0-9]/", $data) > 0) {
+        $errors['password'][] ='&#10005; password must contain a least one alphanumeric<br>';
+    }
+    if (!preg_match("/[#%&¤_\*\-\+\@\!\?\(\)\[\]\$£€]/", $data) > 0) {
+        $errors['password'][] = '&#10005; password must contain a least one special character<br>';
+    }
+    if (!preg_match("/^.{8,40}$/", $data) > 0) {
+        $errors['password'][] = '&#10005; password must at least 8 character long<br>';
+    }
 }
 function validateEmail($data)
 {
-   
+    global $errors;
+   if (!filter_var($data, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'][] = "<p>&#10005;Invalid email format</p>";
+    }
 }
 
-
+function showErrors($type) {
+    global $errors;
+    echo "<p>";
+    foreach ($errors[$type] as $error) {
+        echo $error;
+    }
+    echo "</p>";
+}
 // Om data finns
 if ($username && $password && $email) {
     
@@ -53,13 +77,21 @@ if ($username && $password && $email) {
         <form action="#" method="POST">
             <label>Username <input type="text" name="username"></label>
             <?php
-            echo $resultat;
+            showErrors('username');
             ?>
             <label>Password <input type="password" name="password"></label>
+            <?php
+            showErrors('password');
+            ?>
             <label>Email<input type="email" name="email"></label>
+            <?php
+            showErrors('email');
+            ?>
+            <br>
             <button>Submit</button>
 
         </form>
+        
     </div>
 </body>
 </html>
