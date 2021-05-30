c9931ba1b26030a30925f1cc462d7e78440c9815
LiwiaMa <liwiamatuszczak@gmail.com>
2021-05-30 09:57:30 +0200

    slutprojekt

--- slutprojekt/index.js
+++ slutprojekt/index.js
@@ -1,12 +1,12 @@
 // Välj ut elemet 
-const eKnapp = document.querySelector("#sign-up");
+const eKnapp = document.querySelector("#log-in");
 const eModal = document.querySelector("#geoModal");
 
 // När man klickar på knappen
 eKnapp.addEventListener("click", function() {
     console.log("Hämtar...");
 
-     fetch("./db/registrera.php")
+     fetch("./db/log.in.php")
      .then(response => response.text())
     .then(data => {
         console.log(data);
